7641f81690af1ce58c8791f664c6bee2b7ff9934
RasmusThorsen <rallethor@hotmail.com>
2018-05-17 13:09:01 +0200

    CollisionController for TeamMode

--- TankSprint-3/Classes/TeamCollisionController.cs
+++ TankSprint-3/Classes/TeamCollisionController.cs
@@ -29,15 +29,44 @@ namespace TankSprint_3.Classes
                     {
                         if (_teamRed[i].Vehicle.Collider.Intersects(bullet.Collider))
                         {
-
+                            var handler = Collision;
+                            handler?.Invoke(this, new CollisionEventArgs
+                            {
+                                killerTank = _teamBlue[j].Name,
+                                deadTank = _teamRed[i].Name,
+                                teamColor = "blue"
+                            });
                         }
                     }                   
+                }
+            });
+
+            Parallel.For(0, _teamBlue.Count, i =>
+            {
+                for (int j = 0; j < _teamRed.Count; i++)
+                {
+                    foreach (var bullet in _teamRed[j]._bullets)
+                    {
+                        if (_teamBlue[i].Vehicle.Collider.Intersects(bullet.Collider))
+                        {
+                            var handler = Collision;
+                            handler?.Invoke(this, new CollisionEventArgs
+                            {
+                                killerTank = _teamRed[j].Name,
+                                deadTank = _teamBlue[i].Name,
+                                teamColor = "red"
+                            });
+                        }
+                    }
                 }
             });
         }
     }
 
-    internal class CollisionEventArgs
+    public class CollisionEventArgs : EventArgs
     {
+        public string killerTank { get; set; }
+        public string deadTank { get; set; }
+        public string teamColor { get; set; }
     }
 }
